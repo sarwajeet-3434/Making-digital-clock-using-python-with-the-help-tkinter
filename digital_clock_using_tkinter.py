531bed732195615d00b5ed902215616f350b66bb
Sarwajeet Pratap Singh <121706916+sabboo-3434@users.noreply.github.com>
2023-02-25 11:41:53 +0530

    Add files via upload

---
+++ digital_clock_using_tkinter.py
@@ -0,0 +1,29 @@
+#importing whole module
+from tkinter import *
+from tkinter.ttk import *
+
+#importing strftime function to
+#retrieve system's time
+from time import strftime
+
+#creating tkinter window
+root = Tk() 
+root.title('Clock') 
+
+#This function is used to
+#display time on the label
+def time(): 
+    string = strftime('%H:%M:%S %p') 
+    lbl.config(text = string) 
+    lbl.after(1000, time)
+#styling the label widget so that clock
+#will look more attractive
+lbl = Label(root, font = ('times new roman', 40, 'italic'), 
+            background = 'black', 
+            foreground = 'white')
+
+#Placing clock at the centre
+#of the tkinter window
+lbl.pack(anchor = 'center') 
+time()  
+mainloop()
